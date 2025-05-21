747bfa8764ca41556ba74d1cb4b176c3c67d71f3
VoidRayA <dbwlrltk@naver.com>
2025-05-21 17:49:30 +0900

    20250521

---
+++ resume/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>☎ Contacts : 010-7549-5210 | dbwlrltk@naver.com</p>
+      <p>2025 유지현. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer;
