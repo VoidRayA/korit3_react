a8095dc8bb4af13340777a1fe1e81cde8a964d68
VoidRayA <dbwlrltk@naver.com>
2025-05-27 17:19:18 +0900

    feat: 20250527

--- ch5/routerapp/src/App.tsx
+++ ch5/routerapp/src/App.tsx
@@ -1,9 +1,30 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import PageNotFound from './Components/PageNotFound';
+import ContactBusan from './Components/ContactBusan';
+import './App.css';
+
 
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          { ' | ' }
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+            <Route path="contact" element={<Contact />} >
+              <Route path="busan" element={<ContactBusan />} />
+          </Route>
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
