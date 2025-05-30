aa6163ae40d16c69bc9c22bc884f36bef80317ff
VoidRayA <dbwlrltk@naver.com>
2025-05-30 10:38:29 +0900

    feat

--- ch6/carfront/src/components/Carlist.tsx
+++ ch6/carfront/src/components/Carlist.tsx
@@ -1,14 +1,17 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getCars, deleteCar } from "../api/carapi";
 import { DataGrid, GridColDef, GridCellParams } from "@mui/x-data-grid";
-import { Snackbar, IconButton } from "@mui/material";
+import { Snackbar, IconButton, Button, Stack } from "@mui/material";
 import { useState } from "react";
 import AddCar from "./AddCar";
 import EditCar from "./EditCar";
 import DeleteIcon from '@mui/icons-material/Delete';
 
+type CarlistProps = {
+  logOut?: () => void;
+}
 
-export default function Carlist(){
+export default function Carlist({ logOut }: CarlistProps){
   const queryClient = useQueryClient();
 
   const [ open, setOpen ] = useState(false);
@@ -75,7 +78,11 @@ const { mutate } = useMutation(deleteCar, {
   else {
     return (
       <>
-        <AddCar />
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <AddCar />
+          <Button variant="outlined" onClick={logOut}>LOG OUT</Button>
+        </Stack>
+
         <DataGrid 
           rows={data}
           columns={columns}
